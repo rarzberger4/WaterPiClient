8b287648ea22fc05fdf6b917660ccc2cad8df082
rarzberger4 <91483040+rarzberger4@users.noreply.github.com>
2022-09-13 20:14:57 +0200

    request data form WienerLinien API and only get specified data

--- app.py
+++ app.py
@@ -8,11 +8,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    url = "https://www.wienerlinien.at/ogd_realtime/monitor?rbl=123"
+    url = "https://www.wienerlinien.at/ogd_realtime/monitor?rbl=4619"
     response = urllib.request.urlopen(url)
     data = response.read()
     text = json.loads(data)
-    return text
+    return text['data']['monitors'][0]['locationStop']['properties'][
+        'title']  # getting only the needed data from the json file
 
 
 if __name__ == '__main__':
