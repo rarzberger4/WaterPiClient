c0da1127cc3c275276787d449aa1b578ec847a65
rarzberger4 <91483040+rarzberger4@users.noreply.github.com>
2022-09-24 14:24:20 +0200

    added waterpi http request  - requesting test-schedule

--- app.py
+++ app.py
@@ -19,5 +19,15 @@ def hello_world():  # put application's code here
         'title']  # getting only the needed data from the json file
 
 
+@app.route('/waterpi')
+def waterpi():  # put application's code here
+    url = "http://77.237.53.201:12080/api/wateringPlans/1"
+    response = urllib.request.urlopen(url)
+    data = response.read()
+    text = json.loads(data)
+    print(text, file=sys.stderr)
+    return text
+
+
 if __name__ == '__main__':
     app.run(debug=True, port=80, host='0.0.0.0')
